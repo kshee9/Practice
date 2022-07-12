1338998fc1b3df6de152e05d634b8b1154b8d057
kshee9 <kiang12@naver.com>
2022-07-12 15:23:50 +0900

    연습용

--- src/test/java/com/example/prjava/PrJavaApplicationTests.java
+++ src/test/java/com/example/prjava/PrJavaApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.prjava;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PrJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
