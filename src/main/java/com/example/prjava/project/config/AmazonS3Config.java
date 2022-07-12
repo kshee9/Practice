a4e14c1b6e15cd06bf53cde5963ad2a7e1de1f60
kshee9 <kiang12@naver.com>
2022-07-12 20:16:17 +0900

    연습용

--- src/main/java/com/example/prjava/project/config/AmazonS3Config.java
+++ src/main/java/com/example/prjava/project/config/AmazonS3Config.java
@@ -11,13 +11,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AmazonS3Config {
 
-    @Value("${AWS_ACCESS_KEY_ID}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${AWS_SECRET_ACCESS_KEY}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${AWS_REGION}")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
